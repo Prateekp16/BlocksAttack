ced1e4024299ad0b9cdf6c68d6753aba41ee881d
Prateekp16 <47525892+Prateekp16@users.noreply.github.com>
2019-08-12 19:32:02 +0530

    level1

---
+++ BlocksAttack/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardforce = 2000f;
+    public float sidewaysforce = 500f;
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+    }
+}
